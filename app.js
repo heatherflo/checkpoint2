78a6ceb822ca244499a0eada33d9fe621f55a07c
Heather Floyd <heatherflo@gmail.com>
2023-12-06 20:29:55 -0700

    all upgrades load and calculate correctly

--- app.js
+++ app.js
@@ -14,261 +14,124 @@ const upgrades = [{
   name: "popcorn",
   price: 40,
   quantity: 0,
-  multiplier: 1,
-}]
-
-let automaticUpgrades = [{
-  name: "hotdog",
-  price: 60,
-  quantity: 1,
-  multiplier: 20,
+  multiplier: 5,
 },
 {
-  name: "friend",
-  price: 200,
-  quantity: 2,
-  multiplier: 100,
-}
+  name: "hotdog",
+  price: 60,
+  quantity: 0,
+  multiplier: 10,
+}]
+
+let automaticUpgrades = [
+  {
+    name: "friend",
+    price: 200,
+    quantity: 1,
+    multiplier: 100,
+  }
 ]
 
-
+//my goal is to upgrade my applause points by 1 every time I click on it
 function applause() {
-  points += 10
-  drawApplause()
+  points += 1
+  //I need to run my upgraded applause (to make it calculate) before I can draw the applause points to the page
   upgradedApplause()
+  drawApplause()
 }
 
+//this takes the applause points and updates it to the DOM
 function drawApplause() {
   document.getElementById('applause').innerText = points.toString()
 }
 
+
 function buyItem(itemName) {
+  //finding the item based on its array and then finding the individual name of the item I want to draw out -what ever value I put here (itemName) it will find in the array and then stop looking until it has found all the items that match that name- THAT name is where I call it in the HTML inside the function on the onclick.
   let boughtItem = upgrades.find(item => item.name == itemName)
+  //all the things that happen to my item once I have bought it
   if (points >= boughtItem.price) {
     points -= boughtItem.price,
       boughtItem.quantity += 1,
       boughtItem.price += 5,
       console.log(boughtItem, 'item bought')
-
+    //I want to draw my total upgrades here bc this needs to run before it can upgrade the applause points
+    drawTotalForClickUpgrades()
   }
-  // drawItem()
+  //every time I want to buy an item I need to draw the item to the page and update my applause points. 
   drawApplause()
+  drawItem()
+}
+
+//drawing item to DOM
+function drawItem() {
+  console.log('draw item')
+  //find the individual item from the appropriate array
+  upgrades.forEach(item => {
+    // change the look of the element by grabbing its ID and adding its name to the HTML
+    let itemElm = document.getElementById(item.name)
+    console.log(itemElm, "itemElm")
+    //change the look of the element by grabbing its 'class' and changing its HTML to look like the innerText
+    let statsElm = itemElm.querySelector('.stats')
+    console.log(statsElm, "statsElm")
+    // @ts-ignore
+    statsElm.innerText = `quantity +${item.quantity}`
+    //change the look of the element by grabbing its 'class' and changing its price in the innerText
+    let priceElm = itemElm.querySelector('.price')
+    // @ts-ignore
+    priceElm.innerText = item.price
+
+  })
+
 
 }
 
-// 
-// function drawItem(itemName) {
-// console.log('draw item')
-//NOTE tried updating the DOM in two different ways- query selector and via a template to the HTML
-
-// upgrades.forEach(item => {
-//   let itemElm = document.getElementById(item.name)
-//   console.log(itemElm, "itemElm")
-//   let statsElm = itemElm.querySelector('stats')
-//   console.log(statsElm, "statsElm")
-//   // @type {HTMLDivElement}
-//   // @ts-ignore
-//   statsElm.innerText = `quantity +${item.quantity}`
-// })
-//NOTE this isn't working because the IDs are not individual to each other 
-// let template = ""
-// upgrades.forEach(item => {
-//   if (item.quantity > 0) {
-//     template += `beer<span class="mdi mdi-glass-mug"></span>
-//     <div>
-//       ${item.price}
-//       <span class="mdi mdi-hand-clap"></span>
-//     </div>
-//     <div  class="smallText">
-//       quantity +${item.quantity}
-//     </div>`
-//   }
-// })
-
-// document.getElementById('drawItem').innerHTML = template
-// drawApplause()
-// }
-
+//this calculates how many points my applause goes up based on the multiplier I have. The upgraded points are the NEW points now as I move through the game. 
 function upgradedApplause() {
   console.log('upgraded applause', '🙌🏻')
   let upgradedPoints = 0
   upgrades.forEach(upgrade => {
-    //TODO what do I put here??
-    if ('quantity goes up by one') {
-      upgradedPoints += upgrade.price * upgrade.multiplier
-    }
-  })
-  // console.log(upgradedPoints, 'upgraded points')
-  drawApplause()
-  //NOTE need to change the points logged on the DOM on the applause points bar
 
-  // upgrades.forEach(upgrade => points += upgrade.multiplier * upgrade.quantity)
-  // console.log(upgrades, 'upgrades')
+    upgradedPoints += upgrade.quantity * upgrade.multiplier
+  })
+  points += upgradedPoints
+
+}
+
+//drawing NEW total to the DOM
+function drawTotalForClickUpgrades() {
+  //this is the total for the ENTIRE upgrades and does the same thing as upgrade applause 
+  let upgradedPoints = 1
+  upgrades.forEach(upgrade => {
+
+    upgradedPoints += upgrade.quantity * upgrade.multiplier
+  })
+
+  let clickTotalElm = document.getElementById('clickTotal')
+  clickTotalElm.innerText = upgradedPoints.toString()
+}
+
+
+//this is collecting the auto upgrades and calculating the NEW total based on the points it generates. 
+function collectAuto() {
+  let total = 0
+
+  automaticUpgrades.forEach(upgrade => total += upgrade.quantity * upgrade.multiplier)
+
+  points += total
+  drawApplause()
+}
+
+function drawAuto() {
+
 }
 
 
 
 
+setInterval((collectAuto), 3000)
 
 
 
 
 
-
-
-// function applause() {
-//   console.log("applause", points)
-//   points += 10
-//   // TODO need to count out clicks
-//   // What is my paycheck?
-
-
-
-//   updateApplause()
-// }
-
-// function autoApplause() {
-//TODO like applause, counts up auto upgrades separate "auto paycheck"
-// adds to points
-// draws to page
-// }
-
-// function updateApplause() {
-//   document.getElementById("applause").innerText = points.toString();
-//   console.log("updateApplause", points)
-
-// }
-
-// function buyBeer(beer) {
-//   let boughtItem = upgrades.find(item => item.name == beer)
-//   if (points >= boughtItem.price) { boughtItem.quantity += 1 }
-
-//   if (points >= boughtItem.price) {
-//     points -= boughtItem.price
-
-//   }
-
-
-//   //TODO increase price of upgrade
-//   console.log("bought item", boughtItem, points)
-//   // updateItem()
-//   updateApplause()
-//   updateBeer()
-// }
-
-// function updateBeer(beer) {
-
-//   let template = " "
-//   //FIXME swap every call with a find, don't forget to save it out like you did on line 60
-//   let boughtItem = upgrades.find(item => item.name == beer)
-//   upgrades.find(item => {
-//     if (boughtItem.quantity >= 1) {
-//       template += `<span>quantity + ${item.quantity}`
-//     }
-
-
-//   })
-//   document.getElementById('buyBeer').innerHTML = template
-
-
-// }
-
-
-
-
-// function buyPopcorn(popcorn) {
-//   let boughtItem = upgrades.find(item => item.name == 'popcorn')
-//   if (points >= boughtItem.price) { boughtItem.quantity += 1 }
-
-//   if (points >= boughtItem.price) {
-//     points -= boughtItem.price
-//   }
-//   console.log("bought item", boughtItem, points)
-//   // updateItem()
-//   updatePopcorn()
-//   updateApplause()
-
-// }
-
-// function updatePopcorn() {
-//   let template = " "
-//   upgrades.find(popcorn => {
-//     if (popcorn.quantity >= 1) {
-//       template += `quantity <span>+ ${popcorn.quantity}`
-//     }
-//   })
-//   document.getElementById('buyPopcorn').innerHTML = template
-// }
-
-// function buyBoost(hotdog) {
-
-//   let boughtBoost = automaticUpgrades.find(item => item.name == 'hotdog')
-//   console.log("auto upgrades", boughtBoost)
-//   if (points >= boughtBoost.price) { boughtBoost.quantity += 1 }
-
-//   if (points >= boughtBoost.price) { points -= boughtBoost.price }
-//   updateBoost()
-//   updateApplause()
-
-// }
-
-
-// function updateBoost() {
-//   //this will enable the page to showcase the updated quantity and price of each item (beer or popcorn)
-//   let template = " "
-//   automaticUpgrades.forEach(hotdog => {
-//     if (hotdog.quantity >= 1) {
-//       template += `<span>+ ${hotdog.quantity}</span>`
-//     }
-//   })
-//   document.getElementById('buyHotdog').innerHTML = template
-
-
-// }
-
-// function buyFriend(friend) {
-//   let boughtFriend = automaticUpgrades.find(item => item.name == 'friend')
-//   if (points >= boughtFriend.price) { boughtFriend.quantity += 1 }
-
-//   if (points >= boughtFriend.price) { points -= boughtFriend.price }
-
-
-//   // setInterval(applause, 3000)
-
-
-//   console.log(points, boughtFriend, "friend")
-
-//   updateFriend()
-//   updateApplause()
-// }
-
-// function updateFriend() {
-//   //update friend quantity to the DOM
-
-//   let template = " "
-//   automaticUpgrades.every(friend => {
-//     if (friend.quantity >= 1) {
-//       template += `<span>+ ${friend.quantity}</span>`
-//     }
-//   })
-//   document.getElementById('buyFriend').innerHTML = template
-
-// }
-
-// // function updateItem() {
-
-// //   upgrades.forEach(item => {
-
-// //     let updatedItem = document.getElementById(item.name)
-
-// //     let itemStats = updatedItem.querySelector('.stats')
-
-
-// //     itemStats.innerHTML = `quantity +${item.quantity}`
-// //   })
-// // }
-
-
-
-// //TODO should call autoApplause
-// setInterval(applause, 5000)
